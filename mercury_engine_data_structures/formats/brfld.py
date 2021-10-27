e991087710d81c067600cc00b11c36e799b63e5d
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-10-27 13:58:12 +0300

    More work on BRFLD

--- mercury_engine_data_structures/formats/brfld.py
+++ mercury_engine_data_structures/formats/brfld.py
@@ -1,7 +1,49 @@
-from construct import Struct, Construct, Const, Bytes, CString, Array, GreedyBytes, Int32ul, PrefixedArray
+import construct
+from construct import (
+    Struct, Construct, Const, Bytes, CString, Array, GreedyBytes, Int32ul, PrefixedArray, Int16ul,
+    Switch, Probe, Int64ul, Hex,
+)
 
 from mercury_engine_data_structures.formats import BaseResource
 from mercury_engine_data_structures.game_check import Game
+
+component_types = {
+    'AUDIO': Struct(
+        unk=Array(15, Int16ul),
+    ),
+    'STARTPOINT': Struct(
+        unk=Bytes(0x5d),
+    ),
+    'SCRIPT': Struct(
+        unk=Bytes(0x27),
+    ),
+    'LOGICCAMERA': Struct(
+        unk=Bytes(0x9a),
+    ),
+}
+
+Component = Struct(
+    type=CString("utf-8"),
+    data=Switch(
+        construct.this.type,
+        component_types,
+    )
+)
+
+Object = Struct(
+    name=CString("utf-8"),
+    u7=Array(5, Int32ul),
+    # _=Probe(lookahead=0x20),
+    s7=CString("utf-8"),
+    property_id=Hex(Int64ul),
+    actor_def=CString("utf-8"),
+    u10=Array(14, Int32ul),
+
+    # num_components=Int32ul,
+    # component_0=Component,
+    components=PrefixedArray(Int32ul, Component),
+    _2=Probe(lookahead=0x20),
+)
 
 BRFLD = Struct(
     intro=Bytes(0x33),
@@ -19,27 +61,11 @@ BRFLD = Struct(
 
     maybe_object_count=Int32ul,
 
-    first_object=Struct(
-        name=CString("utf-8"),
-        u7=Array(5, Int32ul),
-        s7=CString("utf-8"),
-        u8=Int32ul,
-        u9=Int32ul,
-        actor_def=CString("utf-8"),
-        u10=Array(15, Int32ul),
-    ),
-
-    # second_object=Struct(
-    #     name=CString("utf-8"),
-    #     u7=Array(5, Int32ul),
-    #     s7=CString("utf-8"),
-    #     u8=Int32ul,
-    #     u9=Int32ul,
-    #     actor_def=CString("utf-8"),
-    #     u10=Array(15, Int32ul),
-    # ),
+    first_object=Object,
+    # second_object=Object,
 
     # s9=CString("utf-8"),
+    # u11=Array(15, Int16ul),
     # u11=Bytes(0x28),
     # s10=CString("utf-8"),
     # u12=Bytes(0x6a),
