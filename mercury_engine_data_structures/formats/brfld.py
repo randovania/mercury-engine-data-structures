b9f2bffec08afab90f07a742606fc34627d4df80
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-10-28 01:42:06 +0300

    Improve pattern for parsing components

--- mercury_engine_data_structures/formats/brfld.py
+++ mercury_engine_data_structures/formats/brfld.py
@@ -1,18 +1,30 @@
+from typing import Dict, Union, Type
+
 import construct
 from construct import (
     Struct, Construct, Const, Bytes, CString, Array, GreedyBytes, Int32ul, PrefixedArray, Int16ul,
-    Switch, Int64ul, Hex, HexDisplayedInteger, Computed, Float32l, Flag, )
+    Switch, Int64ul, Hex, HexDisplayedInteger, Computed, Float32l, Flag, Probe, Int32sl, Prefixed, )
 
 from mercury_engine_data_structures import resource_names
 from mercury_engine_data_structures.construct_extensions.misc import ErrorWithMessage
 from mercury_engine_data_structures.formats import BaseResource
 from mercury_engine_data_structures.game_check import Game
 
+
+def force_quit(ctx):
+    raise SystemExit(1)
+
+
 properties = {}
+
+StrId = CString("utf-8")
 
 
 def add_prop(name: str, value: Construct):
-    properties[resource_names.all_name_to_property_id()[name]] = value
+    prop_id = resource_names.all_name_to_property_id()[name]
+    if prop_id in properties:
+        raise ValueError(f"Attempting to add {name}, but already present.")
+    properties[prop_id] = value
 
 
 PropertyEnum = construct.Enum(Hex(Int64ul), **{
@@ -26,15 +38,52 @@ PropertyElement = Struct(
     element=Switch(
         construct.this.type,
         properties,
-        ErrorWithMessage("Property id without assigned type"),
+        ErrorWithMessage(lambda ctx: f"Property {ctx.type} ({resource_names.all_property_id_to_name().get(ctx.type)}) "
+                                     "without assigned type"),
     )
 )
 
-add_prop("sLevelID", CString("utf-8"))
-add_prop("sScenarioID", CString("utf-8"))
-add_prop("vLayerFiles", PrefixedArray(Int32ul, CString("utf-8")))
+
+def ConfirmType(name: str):
+    def check(ctx):
+        return ctx[f"{name}_type"] != name
+
+    return construct.If(
+        check,
+        ErrorWithMessage(
+            lambda ctx: f"Expected {name}, got {ctx[f'{name}_type']} ("
+                        f"{resource_names.all_property_id_to_name().get(ctx[f'{name}_type'])}) "
+                        "without assigned type"
+        ),
+    )
+
+
+def create_struct(fields: Dict[str, Union[Construct, Type[Construct]]], debug=False):
+    r = [
+        "field_count" / Int32ul,
+    ]
+    for name, subcon in fields.items():
+        r.extend([
+            f"{name}_type" / PropertyEnum,
+            f"_{name}_check" / ConfirmType(name),
+            name / subcon,
+        ])
+
+    if debug:
+        r.extend([
+            "next_enum" / PropertyEnum,
+            "probe" / Probe(lookahead=0x8),
+            "quit" / ErrorWithMessage(force_quit),
+        ])
+
+    return Struct(*r)
+
+
+add_prop("sLevelID", StrId)
+add_prop("sScenarioID", StrId)
+add_prop("vLayerFiles", PrefixedArray(Int32ul, StrId))
 add_prop("rEntitiesLayer", PrefixedArray(Int32ul, PropertyElement))
-add_prop("sName", CString("utf-8"))
+add_prop("sName", StrId)
 
 component_types = {
     'AUDIO': Struct(
@@ -52,7 +101,7 @@ component_types = {
 }
 
 Component = Struct(
-    type=CString("utf-8"),
+    type=StrId,
     data=Switch(
         construct.this.type,
         component_types,
@@ -62,12 +111,12 @@ Component = Struct(
 CVector3D = Array(3, Float32l)
 
 Object = Struct(
-    name=CString("utf-8"),
+    name=StrId,
     u7=Array(5, Int32ul),
     # _=Probe(lookahead=0x20),
-    s7=CString("utf-8"),
+    s7=StrId,
     property_id=PropertyEnum,
-    actor_def=CString("utf-8"),
+    actor_def=StrId,
     u10=Array(14, Int32ul),
 
     # num_components=Int32ul,
@@ -81,14 +130,14 @@ def make_dict(value: Construct, single=True):
         return Struct(
             count=Int32ul,
             value=Struct(
-                key=CString("utf-8"),
+                key=StrId,
                 value=value,
             )
         )
     return PrefixedArray(
         Int32ul,
         Struct(
-            key=CString("utf-8"),
+            key=StrId,
             value=value,
         )
     )
@@ -101,51 +150,146 @@ add_prop("dctSublayers", make_dict(Struct(
     dctActors=PropertyElement,
 )))
 
-add_prop("oActorDefLink", CString("utf-8"))
+add_prop("oActorDefLink", StrId)
 add_prop("vPos", CVector3D)
 add_prop("vAng", CVector3D)
 add_prop("bEnabled", Flag)
-add_prop("v3Position", CVector3D)
 
-add_prop("CLogicCamera", Struct(
-    field_count=Int32ul,
-
-    f1_type=PropertyEnum,
-    f1=CString("utf-8"),
-
-    # bStatic
-    f2_type=PropertyEnum,
-    f2=Flag,
-
-    # v3Position
-    f3=PropertyElement,
-
-    # v3Dir
-    f4_type=PropertyEnum,
-    f4=CVector3D,
-
-    # fFovX
-    f5_type=PropertyEnum,
-    f5=Float32l,
-
-    # fMinExtraZDist
-    f6_type=PropertyEnum,
-    f6=Float32l,
-
-    # fMaxExtraZDist
-    f7_type=PropertyEnum,
-    f7=Float32l,
-
-    # fDefaultInterp
-    f8_type=PropertyEnum,
-    f8=Float32l,
-))
+add_prop("CLogicCamera", create_struct({
+    "sControllerID": StrId,
+    "bStatic": Flag,
+    "v3Position": CVector3D,
+    "v3Dir": CVector3D,
+    "fFovX": Float32l,
+    "fMinExtraZDist": Float32l,
+    "fMaxExtraZDist": Float32l,
+    "fDefaultInterp": Float32l,
+}))
 
 add_prop("CLogicCameraComponent", Struct(
     count=Int32ul,
     c2=PropertyEnum,
     rLogicCamera=PropertyElement,
 ))
+
+add_prop("CAudioComponent", create_struct({
+    "bWantsEnabled": Flag,
+    "bUseDefaultValues": Flag,
+}))
+
+add_prop("CStartPointComponent", create_struct({
+    "bWantsEnabled": Flag,
+    "bUseDefaultValues": Flag,
+
+    "sOnTeleport": StrId,
+    "sOnTeleportLogicCamera": StrId,
+    "bOnTeleportLogicCameraRaw": Flag,
+
+    "bProjectOnFloor": Flag,
+    "bMorphballMode": Flag,
+    "bSaveGameToCheckpoint": Flag,
+    "bIsBossStartPoint": Flag,
+}))
+
+add_prop("CScriptComponent", create_struct({
+    "bWantsEnabled": Flag,
+    "bUseDefaultValues": Flag,
+}))
+
+add_prop("CWeightActivableMovablePlatformComponent", create_struct({
+    "bWantsEnabled": Flag,
+    "bUseDefaultValues": Flag,
+    "sOnActivatedLuaCallback": StrId,
+}))
+
+add_prop("CRumbleComponent", create_struct({
+    "bWantsEnabled": Flag,
+    "bUseDefaultValues": Flag,
+}))
+
+add_prop("CFXComponent", create_struct({
+    "bWantsEnabled": Flag,
+    "bUseDefaultValues": Flag,
+    "fSelectedHighRadius": Float32l,
+    "fSelectedLowRadius": Float32l,
+}))
+
+add_prop("CCollisionComponent", create_struct({
+    "bWantsEnabled": Flag,
+    "bUseDefaultValues": Flag,
+}))
+
+add_prop("CAnimationNavMeshItemComponent", create_struct({
+    "bWantsEnabled": Flag,
+    "bUseDefaultValues": Flag,
+    "tForbiddenEdgesSpawnPoints": make_dict(Struct(
+        x=ErrorWithMessage("Not implemented"),
+    ), single=False),
+}))
+
+add_prop("CAnimationComponent", create_struct({
+    "bWantsEnabled": Flag,
+    "bUseDefaultValues": Flag,
+}))
+
+add_prop("CModelUpdaterComponent", create_struct({
+    "bWantsEnabled": Flag,
+    "bUseDefaultValues": Flag,
+    "sDefaultModelPath": StrId,
+}))
+
+add_prop("CColliderTriggerComponent", create_struct({
+    "bWantsEnabled": Flag,
+    "bUseDefaultValues": Flag,
+    "bCallEntityLuaCallback": Flag,
+    "iReverb": Int32sl,
+    "iLowPassFilter": Int32sl,
+    "sOnEnable": StrId,
+    "sOnDisable": StrId,
+    "bOnEnableAlways": Flag,
+    "bOnDisableAlways": Flag,
+    "bStartEnabled": Flag,
+    "bCheckAllEntities": Flag,
+    "bPersistentState": Flag,
+    "sSfxType": StrId,
+    "lstActivationConditions": PrefixedArray(
+        Int32ul,
+        Struct(
+            "type" / PropertyEnum,
+            "count" / Int32ul,
+
+            "f_type" / PropertyEnum,
+            "f" / Flag,
+
+            "sCharclasses_type" / PropertyEnum,
+            "sCharclasses" / StrId,
+
+            "bEnabled_type" / PropertyEnum,
+            "bEnabled" / Flag,
+
+            "bAlways_type" / PropertyEnum,
+            "bAlways" / Flag,
+
+            "next" / PropertyEnum,
+
+            Probe(),
+            ErrorWithMessage(force_quit),
+        )
+    ),
+}, debug=True))
+
+# add_prop("CAnimationComponent", create_struct({
+#     "bWantsEnabled": Flag,
+#     "bUseDefaultValues": Flag,
+# }, debug=True))
+
+# bEnabled_ = PropertyEnum,
+# bEnabled = Flag,
+#
+# sOnTeleport_ = PropertyEnum,
+#
+# x = Probe(),
+# z = ErrorWithMessage(force_quit),
 
 add_prop("pComponents", PropertyElement)
 add_prop("base::global::CRntSmallDictionary<base::global::CStrId, CActorComponent*>",
@@ -162,7 +306,7 @@ add_prop("dctActors", make_dict(Struct(
     vAng=PropertyElement,
     pComponents=PropertyElement,
     bEnabled=PropertyElement,
-)))
+), single=False))
 
 BRFLD = Struct(
     intro_a=Const(0x42824DE0BB09EF20, Int64ul),
@@ -184,28 +328,6 @@ BRFLD = Struct(
 
     # dctSublayers
     f5=PropertyElement,
-
-    # f5_type=PropertyEnum,  # dctSublayers
-    # #
-    # # # num element in dict
-    # f5=Int32ul,
-    #
-    # # key
-    # s6=CString("utf-8"),  # expects default
-    # u7=Int32ul,
-    # u8_a=PropertyElement,
-    #
-    # enum_1=PropertyEnum,
-    # maybe_object_count=Int32ul,
-
-    # first_object=Object,
-    # second_object=Object,
-
-    # s9=CString("utf-8"),
-    # u11=Array(15, Int16ul),
-    # u11=Bytes(0x28),
-    # s10=CString("utf-8"),
-    # u12=Bytes(0x6a),
 
     raw=GreedyBytes,
 )
