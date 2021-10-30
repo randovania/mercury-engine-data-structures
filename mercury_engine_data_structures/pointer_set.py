f811f196f6f802d37b16fc3440d82b1bcd40fc06
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-10-30 03:10:25 +0300

    Improve output for pointers

--- mercury_engine_data_structures/pointer_set.py
+++ mercury_engine_data_structures/pointer_set.py
@@ -4,10 +4,27 @@ Helper class to handle objects that contain a pointer to objects of varied types
 from typing import Dict, Union, Type
 
 import construct
-from construct import Construct, Struct, Hex, Int64ul, Computed, Switch
+from construct import Construct, Struct, Hex, Int64ul, Computed, Switch, Adapter
 
 from mercury_engine_data_structures import hashed_names
 from mercury_engine_data_structures.construct_extensions.misc import ErrorWithMessage
+
+
+class PointerAdapter(Adapter):
+    def _decode(self, obj: construct.Container, context, path):
+        ret = construct.Container()
+        ret[hashed_names.all_property_id_to_name()[obj.type]] = obj.ptr
+        return ret
+
+    def _encode(self, obj: construct.Container, context, path):
+        if len(obj) != 0:
+            raise construct.ConstructError(f"Invalid obj, expect only one field got {len(obj)}", path)
+        type_name = list(obj.keys())[0]
+        type_id = hashed_names.all_property_id_to_name()[type_name]
+        return construct.Container(
+            type=type_id,
+            ptr=obj[type_name],
+        )
 
 
 class PointerSet:
@@ -18,7 +35,7 @@ class PointerSet:
         self.types = {}
 
     @classmethod
-    def construct_pointer_for(cls, name: str, conn: Union[Construct, Type[Construct]]) -> Struct:
+    def construct_pointer_for(cls, name: str, conn: Union[Construct, Type[Construct]]) -> Construct:
         ret = cls(name, allow_null=True)
         ret.add_option(name, conn)
         return ret.create_construct()
@@ -29,14 +46,13 @@ class PointerSet:
             raise ValueError(f"Attempting to add {name} to {self.category}, but already present.")
         self.types[prop_id] = value
 
-    def create_construct(self) -> Struct:
+    def create_construct(self) -> Construct:
         fields = {
             prop_id: hashed_names.all_property_id_to_name()[prop_id] / conn
             for prop_id, conn in self.types.items()
         }
-        return Struct(
+        return PointerAdapter(Struct(
             type=Hex(Int64ul),
-            type_name=Computed(lambda ctx: hashed_names.all_property_id_to_name().get(ctx.type)),
             ptr=Switch(
                 construct.this.type,
                 fields,
@@ -44,4 +60,4 @@ class PointerSet:
                     lambda ctx: f"Property {ctx.type} ({hashed_names.all_property_id_to_name().get(ctx.type)}) "
                                 "without assigned type"),
             )
-        )
+        ))
