1f9afb75ffbc6018772720cfd85abb91a9510732
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-09-07 23:16:48 +0300

    Only care about files that are in the pkgs

--- tools/list_known_paths_in_toc.py
+++ tools/list_known_paths_in_toc.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 from mercury_engine_data_structures import cli, samus_returns_data, dread_data
 from mercury_engine_data_structures.formats import Toc
+from mercury_engine_data_structures.formats.pkg import PKGHeader, FileEntry
 from mercury_engine_data_structures.game_check import Game
 
 
@@ -25,7 +26,18 @@ def main():
     else:
         raise ValueError(f"unsupported game {args.game}")
 
+    used_asset_id = set()
+
+    for pkg_path in game_root.rglob("*.pkg"):
+        with pkg_path.open("rb") as f:
+            # print(f"\n\n=== {pkg_path.relative_to(game_root)}")
+            header = PKGHeader.parse_stream(f, target_game=args.game)
+            for file_entry in header.file_entries:
+                used_asset_id.add(file_entry.asset_id)
+
     for asset_id in toc.get_all_asset_id():
+        if asset_id not in used_asset_id:
+            continue
         print("{:016x}: {}".format(
             asset_id,
             id_mapping.get(asset_id, ""),
