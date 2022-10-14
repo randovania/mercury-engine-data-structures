2c91e239c56b96d8cc921250d97c667805d7fb26
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-10-14 20:08:46 +0300

    Simplify object c in Bmsld

--- mercury_engine_data_structures/formats/bmsld.py
+++ mercury_engine_data_structures/formats/bmsld.py
@@ -1,71 +1,9 @@
 import construct
-from construct import Array, Construct, Struct, Const, Int32ul, Int16ul, Int8ul, Hex, CString, Float32l
+from construct import Array, Construct, Struct, Const, Int32ul, Int8ul, Hex, CString, Float32l
 
 from mercury_engine_data_structures.common_types import make_vector
 from mercury_engine_data_structures.formats import BaseResource
 from mercury_engine_data_structures.game_check import Game
-
-type_body=construct.Switch(
-    construct.this.object_type,
-    {
-        0x0400: Struct(
-            unk12=Array(4, Struct(
-                x=Float32l,
-                y=Float32l,
-                z=Int32ul,
-            )),
-        ),
-        0x0600: Struct(
-            unk12=Array(6, Struct(
-                x=Float32l,
-                y=Float32l,
-                z=Int32ul,
-            )),
-        ),
-        0x0800: Struct(
-            unk12=Array(8, Struct(
-                x=Float32l,
-                y=Float32l,
-                z=Int32ul,
-            )),
-        ),
-        0x0A00: Struct(
-            unk12=Array(10, Struct(
-                x=Float32l,
-                y=Float32l,
-                z=Int32ul,
-            )),
-        ),
-        0x0C00: Struct(
-            unk12=Array(12, Struct(
-                x=Float32l,
-                y=Float32l,
-                z=Int32ul,
-            )),
-        ),
-        0x0E00: Struct(
-            unk12=Array(14, Struct(
-                x=Float32l,
-                y=Float32l,
-                z=Int32ul,
-            )),
-        ),
-        0x1400: Struct(
-            unk12=Array(20, Struct(
-                x=Float32l,
-                y=Float32l,
-                z=Int32ul,
-            )),
-        ),
-        0x1C00: Struct(
-            unk12=Array(28, Struct(
-                x=Float32l,
-                y=Float32l,
-                z=Int32ul,
-            )),
-        ),
-    }
-)
 
 BMSLD = Struct(
     _magic=Const(b"MSLD"),
@@ -106,10 +44,15 @@ BMSLD = Struct(
         unk06=Hex(Int32ul),
         unk07=Hex(Int32ul),
         unk08=Hex(Int32ul),
-        object_type=Hex(Int16ul),
-        unk10=Hex(Int32ul),
+        unk09=Hex(Int8ul),
+        count_for_unk12=Hex(Int32ul),
+        unk10=Hex(Int8ul),
         unk11=Hex(Int32ul),
-        unk12=type_body,
+        unk12=Array(construct.this.count_for_unk12, Struct(
+            x=Float32l,
+            y=Float32l,
+            z=Int32ul,
+        )),
         unk13=Array(4, Struct(
             x=Float32l,
             y=Float32l,
