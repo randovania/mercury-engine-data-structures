49a2699647d5de85eda1ca29d08dd4efd1dac87c
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-10-14 21:34:08 +0300

    Progress in bmsld

--- mercury_engine_data_structures/formats/bmsld.py
+++ mercury_engine_data_structures/formats/bmsld.py
@@ -1,11 +1,25 @@
 import construct
-from construct import Array, Construct, Struct, Const, Int32ul, Int8ul, Hex, CString, Float32l, Flag
+from construct import Array, Construct, Struct, Const, Int32ul, Int8ul, Hex, CString, Float32l, Flag, Int16ul
 
 from mercury_engine_data_structures.common_types import make_vector, StrId, UInt, Float
 from mercury_engine_data_structures.construct_extensions.misc import ErrorWithMessage
 from mercury_engine_data_structures.formats import BaseResource
 from mercury_engine_data_structures.formats.bmscc import CollisionPoly, CollisionPoint
 from mercury_engine_data_structures.game_check import Game
+
+FunctionArgument = Struct(
+    type=construct.PaddedString(4, 'ascii'),
+    value=construct.Switch(
+        construct.this.type,
+        {
+            's': StrId,
+            'f': Float,
+            'b': Flag,
+            'i': Int32ul,
+        },
+        ErrorWithMessage(lambda ctx: f"Unknown argument type: {ctx.type}", construct.SwitchError)
+    )
+)
 
 BMSLD = Struct(
     _magic=Const(b"MSLD"),
@@ -88,7 +102,8 @@ BMSLD = Struct(
                     polys=make_vector(Struct(
                         num_points=UInt,
                         unk=Float,
-                        points=Array(construct.this.num_points, Struct(x=Hex(UInt), y=Hex(UInt), material_attribute=Hex(UInt))),
+                        points=Array(construct.this.num_points,
+                                     Struct(x=Hex(UInt), y=Hex(UInt), material_attribute=Hex(UInt))),
                         loop=Flag,
                         boundings=Array(4, Float),
                     )),
@@ -104,6 +119,56 @@ BMSLD = Struct(
     )),
 
     objects_count=Int32ul,
+
+    objects_e_1=Struct(
+        name=StrId,
+        unk01=StrId,
+        unk02=Hex(Int32ul),
+        unk03=Hex(Int32ul),
+        unk04=Hex(Int32ul),
+        unk05=Hex(Int32ul),
+        unk06=Hex(Int32ul),
+        unk07=Hex(Int32ul),
+        unk08=Hex(Int32ul),
+        unk09=Float,
+        unk10=Float,
+        unk11=Hex(Int32ul),
+
+        # unk12=Hex(Int8ul),
+        unk13=StrId,
+        unk14=Hex(Int32ul),
+        unk15=Hex(Int32ul),
+    ),
+
+    objects_e_2=Struct(
+        name=StrId,
+        unk01=StrId,
+
+        unk02=Hex(Int32ul),
+        unk03=Hex(Int32ul),
+        unk04=Hex(Int32ul),
+        unk05=Hex(Int32ul),
+        unk06=Hex(Int32ul),
+        unk07=Hex(Int32ul),
+
+        components=make_vector(Struct(
+            component_type=StrId,
+            data=construct.Switch(
+                construct.this.component_type,
+                {
+                    "ABOSS": Struct(
+                        unk12=Hex(Int32ul),
+                        unk13=Hex(Int32ul),
+                    ),
+                    "TRIGGER": Struct(
+                        command=StrId,
+                        arguments=make_vector(FunctionArgument),
+                    ),
+                },
+            ),
+        )),
+    ),
+
     rest=construct.Bytes(0x100),
 )
 
