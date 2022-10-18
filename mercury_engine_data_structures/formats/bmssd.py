8047c22c19abf2acbce4567984ed7494323f04c2
dyceron <38679103+dyceron@users.noreply.github.com>
2022-10-18 19:56:10 -0400

    More WIP

--- mercury_engine_data_structures/formats/bmssd.py
+++ mercury_engine_data_structures/formats/bmssd.py
@@ -10,7 +10,6 @@ from mercury_engine_data_structures.formats import BaseResource
 from mercury_engine_data_structures.game_check import Game, current_game_at_most
 
 VectorArray = Array(3, Float32l)
-VectorArray2 = Array(30, Float32l)
 
 BMSSD = Struct(
     _magic=Const(b"MSSD"),
@@ -111,75 +110,60 @@ BMSSD = Struct(
         )
     ),
     # # SR Testing
-    # unk3=Int32ul,
-    # unk4=Int32ul,
-    # unk_structs_b=Struct(
-    #     str1=CString("utf-8"),
-    #     char2=Byte,
-    #     char3=Byte,
-    #     char4=Byte,
-    #     int5=Int32ul,
-    #     int6=Int32ul,
-    #     int7=Int32ul,
-    #     char8=Byte,
-    #     char9=Byte,
-    #     int10=Int32ul,
-    #     # str11=PaddedString(16, "utf-8"),
-    #     int12=Int32ul,
-    #     float13=VectorArray,
-    #     int11=Int8ul,
-    #     float14=VectorArray,
-    #     float15=VectorArray,
-    #     int16=Int32ul,
-    #     float17=VectorArray,
-    #     float18=VectorArray2,
-    #     float19=VectorArray2,
-    #     float20=VectorArray2,
-    #     float21=VectorArray2,
-    #     float22=VectorArray2,
-    #     float23=VectorArray2,
-    #     float24=VectorArray2,
-    #     float25=VectorArray2,
-    #     float26=VectorArray2,
-    #     float27=VectorArray2,
-    #     float28=VectorArray2,
-    #     float29=VectorArray2,
-    #     float30=VectorArray2,
-    #     float31=VectorArray2,
-    #     float32=VectorArray2,
-    #     float33=VectorArray2,
-    #     float34=VectorArray2,
-    #     float35=VectorArray2,
-    #     float36=VectorArray2,
-    #     float37=VectorArray2,
-    #     float38=VectorArray2,
-    #     float39=VectorArray2,
-    #     float40=VectorArray2,
-    #     float41=VectorArray2,
-    #     float42=VectorArray2,
-    #     float43=VectorArray2,
-    #     float44=VectorArray2,
-    #     float45=VectorArray2,
-    #     float46=VectorArray2,
-    #     float47=VectorArray2,
-    #     float48=VectorArray2,
-    #     float49=VectorArray,
-    #     float50=VectorArray,
-    #     float51=VectorArray,
-    #     float53=VectorArray,
-    #     float54=VectorArray,
-    #     float55=VectorArray,
-    #     float56=VectorArray,
-    #     float57=VectorArray,
-    #     float58=VectorArray,
-    #     float59=VectorArray,
-    #     float60=VectorArray,
-    #     float61=VectorArray,
-    #     float62=VectorArray,
-    #     unk16=Hex(Int32ul),
-    #     unk17=Hex(Int32ul),
-    #     unk18=Hex(Int16ul),
-    # )[1],
+    unk3=Int32ul,
+    unk4=Int32ul,
+    strings_b=Struct(
+        str1=CString("utf-8"),
+        char2=Byte,
+        char3=Byte,
+        char4=Byte,
+        int5=Int32ul,
+        float6=VectorArray,
+        int7=Int8ul,
+    )[3],
+    # strings_b=PrefixedArray(
+    #     Int32ul,
+    #     Struct(
+    #         str1=CString("utf-8"),
+    #         char2=Byte,
+    #         char3=Byte,
+    #         char4=Byte,
+    #         int5=Int32ul,
+    #         float6=VectorArray,
+    #         int7=Int8ul,
+    #     ),
+    # ),
+    # strings_b=PrefixedArray(
+    #     Int32ul,
+    #     CString("utf-8"),
+    # ),
+    unk_structs_b=Struct(
+        str1=CString("utf-8"),
+        char2=Byte,
+        char3=Byte,
+        char4=Byte,
+        int5=Int32ul,
+        int6=Int32ul,
+        int7=Int32ul,
+        char8=Byte,
+        char9=Byte,
+        int10=Int32ul,
+        # str11=PaddedString(16, "utf-8"),
+        int12=Int32ul,
+        float13=VectorArray,
+        int11=Int8ul,
+        float14=VectorArray,
+        float15=VectorArray,
+        int16=Hex(Int32ul),
+        float17=VectorArray,
+        elems=Struct(
+            float1=VectorArray,
+            float2=VectorArray,
+            float3=VectorArray,
+        )[55],
+        int18=Hex(Int32ul),
+        int19=Int16ul,
+    )[1],
     # unk_structs_c=Struct(
     #     str1=CString("utf-8"),
     #     char2=Byte,
@@ -199,24 +183,20 @@ BMSSD = Struct(
     #     int16=Int32ul,
     #     float17=VectorArray,
     # ),
-    strings_b=PrefixedArray(
-        Int32ul,
-        CString("utf-8"),
-    ),
-    unk_structs_b=PrefixedArray(
-        Int32ul,
-        Struct(
-            str1=CString("utf-8"),
-            int2=Int32ul,
-            struct4=PrefixedArray(
-                Int32ul,
-                Struct(
-                    int1=Int32ul,
-                    long3=PrefixedArray(Int32ul, Int64ul),
-                )
-            )
-        )
-    ),
+    # unk_structs_b=PrefixedArray(
+    #     Int32ul,
+    #     Struct(
+    #         str1=CString("utf-8"),
+    #         int2=Int32ul,
+    #         struct4=PrefixedArray(
+    #             Int32ul,
+    #             Struct(
+    #                 int1=Int32ul,
+    #                 long3=PrefixedArray(Int32ul, Int64ul),
+    #             )
+    #         )
+    #     )
+    # ),
     rest=construct.GreedyBytes,
 )
 
