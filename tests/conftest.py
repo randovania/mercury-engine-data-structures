6847b4d6e311a1289c2f3bb2ba6685b9f4801915
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2023-07-06 01:23:19 +0300

    Add --fail-if-missing

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,10 +6,15 @@ import pytest
 from mercury_engine_data_structures.file_tree_editor import FileTreeEditor
 from mercury_engine_data_structures.game_check import Game
 
+_FAIL_INSTEAD_OF_SKIP = False
+
 
 def get_env_or_skip(env_name):
     if env_name not in os.environ:
-        pytest.skip(f"Skipped due to missing environment variable {env_name}")
+        if _FAIL_INSTEAD_OF_SKIP:
+            pytest.fail(f"Missing environment variable {env_name}")
+        else:
+            pytest.skip(f"Skipped due to missing environment variable {env_name}")
     return os.environ[env_name]
 
 
@@ -27,3 +32,12 @@ def dread_path():
 def dread_file_tree(dread_path):
     return FileTreeEditor(dread_path, Game.DREAD)
 
+
+def pytest_addoption(parser):
+    parser.addoption('--fail-if-missing', action='store_true', dest="fail_if_missing",
+                     default=False, help="Fails tests instead of skipping, in case any asset is missing")
+
+
+def pytest_configure(config: pytest.Config):
+    global _FAIL_INSTEAD_OF_SKIP
+    _FAIL_INSTEAD_OF_SKIP = config.option.fail_if_missing
