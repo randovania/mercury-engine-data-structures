7e8e205d11c171698b0eb1c02a6ccb5532ec4e3f
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2023-08-01 21:30:48 +0300

    Add compile to construct.Hex

--- src/mercury_engine_data_structures/construct_extensions/__init__.py
+++ src/mercury_engine_data_structures/construct_extensions/__init__.py
@@ -5,3 +5,12 @@ import construct.expr
 # Workaround construct's bug (See issue #1039)
 construct.expr.opnames[operator.and_] = "&"
 construct.expr.opnames[operator.or_] = "|"
+
+
+# Hex for some reason doesn't support compilation for building, despite being trivial to do
+# So let's hack it in.
+def _hex_emitbuild(self, code):
+    return self.subcon._compilebuild(code)
+
+
+construct.Hex._emitbuild = _hex_emitbuild
