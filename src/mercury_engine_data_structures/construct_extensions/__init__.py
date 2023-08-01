c93736f749fb1b87c7d5f69e7d79d05693cd82b7
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2023-08-01 21:41:43 +0300

    Add explicit link

--- src/mercury_engine_data_structures/construct_extensions/__init__.py
+++ src/mercury_engine_data_structures/construct_extensions/__init__.py
@@ -2,7 +2,7 @@ import operator
 
 import construct.expr
 
-# Workaround construct's bug (See issue #1039)
+# Workaround construct's bug (See https://github.com/construct/construct/issues/1039)
 construct.expr.opnames[operator.and_] = "&"
 construct.expr.opnames[operator.or_] = "|"
 
