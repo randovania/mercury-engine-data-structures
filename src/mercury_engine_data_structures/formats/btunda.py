3ffdd4b7e0e6667e98a27997e90422096fce5ea3
Steven Franklin <steve11sjf@gmail.com>
2023-08-05 15:38:13 -0500

    fixed version checking

--- src/mercury_engine_data_structures/formats/btunda.py
+++ src/mercury_engine_data_structures/formats/btunda.py
@@ -1,26 +1,14 @@
 import construct
 
-from construct import Container
+from construct import Container, Struct
 from enum import Enum
 from mercury_engine_data_structures.formats import BaseResource, standard_format
 from mercury_engine_data_structures.game_check import Game
 
-# btunda has different file versions between launch version and latest release
-class BtundaVersion(Enum):
-    V1_0_0 = ("1.0.0", 0x02000077)
-    V2_1_0 = ("2.1.0", 0x02000080)
-
-    def __init__(self, string: str, number: int):
-        self.as_string = string
-        self.as_number = number
-
-    @classmethod
-    def get_version(cls, version: str) -> int:
-        for el in cls:
-            if version == el.as_string:
-                return el.as_number
-        
-        raise ValueError(f"Version {version} is not a valid Btunda version!")
+VALID_BTUNDA_VERSIONS = [
+    0x02000077, # 1.0.0
+    0x02000080, # 2.1.0
+]
 
 class Btunda(BaseResource):
     """
@@ -32,24 +20,22 @@ class Btunda(BaseResource):
     - any key containing EasyMode in Player|CTunablePlayerLifeComponent\n
     - anything in CTunableBossRushManager
     """
+
     @classmethod
     def construct_class(cls, target_game: Game, version:int) -> construct.Construct:
         return standard_format.create('base::tunable::CTunableManager', version)
 
     @classmethod
     def parse(cls, data: bytes, target_game: Game) -> "BaseResource":
-        # attempt to parse with 1.0.0 version
-        # if it throws an exception, parse with 2.1.0 version
-        try:
-            parsed = cls(Btunda.construct_class(target_game, BtundaVersion.V1_0_0.as_number).parse(data, target_game=target_game),
-                   target_game)
-            parsed.version_str = BtundaVersion.V1_0_0.as_string
-            return parsed
-        except:
-            parsed = cls(Btunda.construct_class(target_game, BtundaVersion.V2_1_0.as_number).parse(data, target_game=target_game),
-                   target_game)
-            parsed.version_str = BtundaVersion.V2_1_0.as_string
-            return parsed
+        # peek at data to get version
+        version = int.from_bytes(data[8:12], byteorder='little')
+
+        # confirm version is valid
+        if version not in VALID_BTUNDA_VERSIONS:
+            raise ValueError((f"BTUNDA version {hex(version.to_bytes(4, byteorder='little'))}"
+                             " is not an implemented BTUNDA version!"))
+        
+        return cls(Btunda.construct_class(target_game, version).parse(data, target_game=target_game), target_game)
     
     def get_tunable(self, tunable: str) -> Container:
         return self.raw.Root.hashTunables[tunable]
