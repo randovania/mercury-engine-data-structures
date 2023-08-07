e496067ed7b5743d8735fcfe67ae82774729e2e2
Steven Franklin <steve11sjf@gmail.com>
2023-08-07 03:22:47 -0500

    Updated btunda PR
    - damn its been a bit aint it
    - implemented VersionAdapter and use construct.Check to validate against
      expected versions
    - added get_tunable which gets a tunable at a given path
    - added set_tunable which guards against setting version-dependent
      tunables on the wrong version
    - added test case for btunda, get_tunable and set_tunable

---
+++ tests/formats/test_btunda.py
@@ -0,0 +1,31 @@
+import pytest
+from tests.test_lib import parse_build_compare_editor
+
+from mercury_engine_data_structures.formats.btunda import Btunda
+
+BTUNDA_PATH = "system/tunables/tunables.btunda"
+def test_btunda(dread_file_tree):
+    parse_build_compare_editor(Btunda, dread_file_tree, BTUNDA_PATH)
+
+def test_btunda_get_tunable(dread_file_tree):
+    btunda = dread_file_tree.get_parsed_asset(BTUNDA_PATH, type_hint=Btunda)
+
+    assert btunda.get_tunable(["ShotManager|CTunableShotManager", "rPlasmaBeamDiffusionExplosion", "uAmount"]) == 3
+
+def test_btunda_set_tunable(dread_file_tree):
+    btunda = dread_file_tree.get_parsed_asset(BTUNDA_PATH, type_hint=Btunda)
+
+    btunda.set_tunable(["SubAreaManager|CTunableSubAreaManager", "bKillPlayerOutsideScenario"], False)
+
+    assert btunda.raw.Root.hashTunables["SubAreaManager|CTunableSubAreaManager"]["bKillPlayerOutsideScenario"] is False
+
+def test_btunda_invalid_tunable(dread_file_tree):
+    btunda = dread_file_tree.get_parsed_asset(BTUNDA_PATH, type_hint=Btunda)
+
+    if btunda.raw.version == "119.0.2":
+        with pytest.raises(ValueError):
+            btunda.set_tunable(["CTunableBossRushManager", "fDeathAddedTime"], 420.0)
+
+    else:
+        with pytest.raises(ValueError):
+            btunda.set_tunable(["CTunableGameManager", "bForceHardMode"], True)
