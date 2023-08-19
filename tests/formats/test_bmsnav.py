23013a67ad8d8b3f927df5a98834928f88445f6c
Steven Franklin <steve11sjf@gmail.com>
2023-08-19 03:09:00 -0500

    BMSNAV parse/build for Samus Returns
    - mostly just a simpler format of Dread's navmesh
    - geo_connections have 3 bytes added to the start of each entry
    - there's an unknown struct that seems to be there for metroids,
      probably similar to how emmy actions are in dread

--- tests/formats/test_bmsnav.py
+++ tests/formats/test_bmsnav.py
@@ -3,7 +3,7 @@ from tests.test_lib import parse_build_compare_editor
 
 from mercury_engine_data_structures.formats.bmsnav import Bmsnav
 
-all_bmsnav = [
+dread_bmsnav = [
     "maps/levels/c10_samus/s010_cave/s010_cave.bmsnav",
     "maps/levels/c10_samus/s020_magma/s020_magma.bmsnav",
     "maps/levels/c10_samus/s030_baselab/s030_baselab.bmsnav",
@@ -15,7 +15,31 @@ all_bmsnav = [
     "maps/levels/c10_samus/s090_skybase/s090_skybase.bmsnav",
 ]
 
+sr_bmsnav = [
+    "maps/levels/c10_samus/s000_surface/s000_surface.bmsnav",
+    "maps/levels/c10_samus/s010_area1/s010_area1.bmsnav",
+    "maps/levels/c10_samus/s020_area2/s020_area2.bmsnav",
+    "maps/levels/c10_samus/s025_area2b/s025_area2b.bmsnav",
+    "maps/levels/c10_samus/s028_area2c/s028_area2c.bmsnav",
+    "maps/levels/c10_samus/s030_area3/s030_area3.bmsnav",
+    "maps/levels/c10_samus/s033_area3b/s033_area3b.bmsnav",
+    "maps/levels/c10_samus/s036_area3c/s036_area3c.bmsnav",
+    "maps/levels/c10_samus/s040_area4/s040_area4.bmsnav",
+    "maps/levels/c10_samus/s050_area5/s050_area5.bmsnav",
+    "maps/levels/c10_samus/s060_area6/s060_area6.bmsnav",
+    "maps/levels/c10_samus/s065_area6b/s065_area6b.bmsnav",
+    "maps/levels/c10_samus/s067_area6c/s067_area6c.bmsnav",
+    "maps/levels/c10_samus/s070_area7/s070_area7.bmsnav",
+    "maps/levels/c10_samus/s090_area9/s090_area9.bmsnav",
+    "maps/levels/c10_samus/s100_area10/s100_area10.bmsnav",
+    "maps/levels/c10_samus/s110_surfaceb/s110_surfaceb.bmsnav",
+]
 
-@pytest.mark.parametrize("bmsnav_path", all_bmsnav)
-def test_bmsnav(dread_file_tree, bmsnav_path):
+
+@pytest.mark.parametrize("bmsnav_path", dread_bmsnav)
+def test_dread_bmsnav(dread_file_tree, bmsnav_path):
     parse_build_compare_editor(Bmsnav, dread_file_tree, bmsnav_path)
+
+@pytest.mark.parametrize("bmsnav_path", sr_bmsnav)
+def test_sr_bmsnav(samus_returns_tree, bmsnav_path):
+    parse_build_compare_editor(Bmsnav, samus_returns_tree, bmsnav_path)
