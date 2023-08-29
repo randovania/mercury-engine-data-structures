7911a0a3441651133ce57949b3745a2f452646fb
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2023-08-29 13:24:38 +0300

    Object: create functions for the switch cases
    By naming based on sub-construct id, we avoid duplicating code
    which saves time when compiling the code

--- src/mercury_engine_data_structures/construct_extensions/function_complex.py
+++ src/mercury_engine_data_structures/construct_extensions/function_complex.py
@@ -1,4 +1,52 @@
+from typing import Dict, Optional, Type, Union
+
 import construct
+
+
+def emit_switch_cases_parse(
+        code: construct.CodeGen,
+        fields: Dict[str, Union[construct.Construct, Type[construct.Construct]]],
+        custom_table_name: Optional[str] = None,
+) -> str:
+    """Construct codegen helper for handling the switch cases dict in _emitparse."""
+    table_name = custom_table_name
+    if table_name is None:
+        table_name = f"parse_switch_cases_{code.allocateId()}"
+
+    block = f"{table_name} = {{\n"
+    for type_name, type_class in fields.items():
+        code.append(f"""
+        def _parse_{id(type_class)}(io, this):
+            return {type_class._compileparse(code)}
+        """)
+
+        block += f"    {repr(type_name)}: _parse_{id(type_class)},  # {type_class.name}\n"
+    block += "}"
+    code.append(block)
+
+    return table_name
+
+
+def emit_switch_cases_build(
+        code: construct.CodeGen,
+        fields: Dict[str, Union[construct.Construct, Type[construct.Construct]]],
+        custom_table_name: Optional[str] = None,
+) -> str:
+    """Construct codegen helper for handling the switch cases dict in _emitbuild."""
+    table_name = custom_table_name
+    if table_name is None:
+        table_name = f"build_switch_cases_{code.allocateId()}"
+
+    block = f"{table_name} = {{\n"
+    for type_name, type_class in fields.items():
+        code.append(f"""
+        def _build_{id(type_class)}(obj, io, this):
+            return {type_class._compilebuild(code)}
+        """)
+        block += f"    {repr(type_name)}: _build_{id(type_class)},  # {type_class.name}\n"
+    block += "}"
+    code.append(block)
+    return table_name
 
 
 class SwitchComplexKey(construct.Switch):
@@ -34,8 +82,8 @@ class ComplexIfThenElse(construct.IfThenElse):
 
     def _emitparse(self, code):
         return "(({}) if ({}) else ({}))".format(self.thensubcon._compileparse(code),
-                                             self._insert_cond(code),
-                                             self.elsesubcon._compileparse(code),)
+                                                 self._insert_cond(code),
+                                                 self.elsesubcon._compileparse(code), )
 
     def _emitbuild(self, code):
         return (f"(({self.thensubcon._compilebuild(code)}) if ("
