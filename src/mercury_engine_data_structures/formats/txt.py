86f2d3b86081d3cc77eed1bd03f8f8d24e4232c2
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2023-08-29 17:39:23 +0300

    Compile TXT

--- src/mercury_engine_data_structures/formats/txt.py
+++ src/mercury_engine_data_structures/formats/txt.py
@@ -1,5 +1,7 @@
+import functools
 from typing import Dict
 
+import construct
 from construct.core import Const, Construct, GreedyRange, Struct
 
 from mercury_engine_data_structures.common_types import DictAdapter, DictElement
@@ -7,16 +9,46 @@ from mercury_engine_data_structures.construct_extensions.strings import CStringR
 from mercury_engine_data_structures.formats import BaseResource
 from mercury_engine_data_structures.game_check import Game
 
+_string_range = GreedyRange(DictElement(CStringRobust("utf-16-le")))
+
+
+def _emitparse(code: construct.CodeGen) -> str:
+    n = code.allocateId()
+    code.append(f"""
+    def _parse_{n}(io, this):
+        result = ListContainer()
+        try:
+            for i in itertools.count():
+                this._index = i
+                fallback = io.tell()
+                result.append({_string_range.subcon._compileparse(code)})
+        except StopFieldError:
+            pass
+        except ExplicitError:
+            raise
+        except Exception:
+            io.seek(fallback)
+        return result
+    """)
+    return f"_parse_{n}(io, this)"
+
+
+_string_range._emitparse = _emitparse
+
+
 TXT = Struct(
     "magic" / Const(b'BTXT'),
     "version" / Const(b'\x01\x00\x0a\x00'),
-    "strings" / DictAdapter(GreedyRange(DictElement(CStringRobust("utf-16-le"))))
+    "strings" / DictAdapter(_string_range),
+    "_end" / construct.Terminated,
 )
+
 
 class Txt(BaseResource):
     @classmethod
+    @functools.lru_cache
     def construct_class(cls, target_game: Game) -> Construct:
-        return TXT
+        return TXT.compile()
 
     @property
     def strings(self) -> Dict[str, str]:
