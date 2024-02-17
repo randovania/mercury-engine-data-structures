f881996c93dc6eacd4e8742911fe33d26901fe3a
Thanatos <thanatosstr89@gmail.com>
2024-02-17 15:19:41 +0100

    Add comment to link sources Add enum for bctex format and mip map entry

--- src/mercury_engine_data_structures/formats/bctex.py
+++ src/mercury_engine_data_structures/formats/bctex.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 import construct
 from construct.core import Error
 
@@ -90,22 +92,30 @@ BCTEX_Dread = construct.Struct(
     ),
 )
 
-PICATextureFormat = construct.Enum(
+class PICATextureFormat(Enum):
+    L8 = 0
+    A8 = 1
+    LA4 = 2
+    LA8 = 3
+    HiLo8 = 4
+    RGB565 = 5
+    RGB8 = 6
+    RGBA5551 = 7
+    RGBA4 = 8
+    RGBA8 = 9
+    ETC1 = 10
+    ETC1A4 = 11
+    L4 = 12
+    A4 = 13
+
+BCTEXFormat = construct.Enum(
     UInt,
-    L8=0,
-    A8=1,
-    LA4=2,
-    LA8=3,
-    HiLo8=4,
-    RGB565=5,
-    RGB8=6,
-    RGBA5551=7,
-    RGBA4=8,
-    RGBA8=9,
-    ETC1=10,
-    ETC1A4=11,
-    L4=12,
-    A4=13,
+    RGB8_0   = 0x10001, # 65537
+    RGBA8_0  = 0x20001, # 65538,
+    L8_0     = 0x40001, # 65540
+    LA8_0    = 0x50001, # 65541
+    ETC1_0   = 0x10003, # 196609
+    ETC1a4_0 = 0x20003  # 196610
 )
 
 CTPK = construct.Struct(
@@ -123,7 +133,7 @@ CTPK = construct.Struct(
         name_offset=UInt,
         image_size=UInt,
         data_offset=UInt,
-        texture_format=PICATextureFormat,
+        texture_format=construct.Enum(UInt, PICATextureFormat),
         width=construct.Int16ul,
         height=construct.Int16ul,
         mip_count=construct.Byte,
@@ -138,7 +148,7 @@ CTPK = construct.Struct(
     hash = UInt,
     _mip_map_entries_begin=construct.Seek(construct.this.file_header.mipmap_entries_offset),
     mip_map_entry= construct.Struct(
-        texture_format = construct.Byte,
+        texture_format = construct.Enum(construct.Byte, PICATextureFormat),
         mip_count = construct.Byte,
         compressed = construct.Byte,
         etc1_quality = construct.Byte,
@@ -146,12 +156,15 @@ CTPK = construct.Struct(
     image_data = construct.Bytes(construct.this.file_header.texture_data_size)
 )
 
+# this and all related structures is a mixture of some own work + most parts taken from:
+# https://github.com/M-1-RLG/010-Editor-Templates/
+# https://github.com/MapStudioProject/CTR-Studio
+# https://github.com/FanTranslatorsInternational/Kuriimu2/
 BCTEX_SR = construct.Struct(
     _magic=construct.Const(b"MTXT"),
     major_version=construct.Int16ul,
     minor_version=construct.Int16ul,
-    # no idea what this value means
-    texture_format=UInt,
+    texture_format=BCTEXFormat,
     width=UInt,
     height=UInt,
     mipmap_count=UInt,
