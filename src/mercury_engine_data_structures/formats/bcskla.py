385226842b79c16f0263b1280f7e9887a8399a4f
Steven Franklin <steve11sjf@gmail.com>
2024-07-18 13:20:36 -0500

    Added Samus Returns BCSKLA parsing and building
    - Samus Returns has its own bcskla parser/builder
    - Added test cases for MSR
    - Fixes #159

--- src/mercury_engine_data_structures/formats/bcskla.py
+++ src/mercury_engine_data_structures/formats/bcskla.py
@@ -1,6 +1,3 @@
-from dataclasses import dataclass
-from pprint import pprint
-from typing import Union
 
 import construct
 from construct.core import (
@@ -10,7 +7,6 @@ from construct.core import (
     Construct,
     Container,
     FocusedSeq,
-    If,
     IfThenElse,
     Int8ul,
     Int16ul,
@@ -18,12 +14,11 @@ from construct.core import (
     Pointer,
     Rebuild,
     Struct,
-    Seek,
     Tell,
     this,
 )
 
-from mercury_engine_data_structures.common_types import CVector2D, Float
+from mercury_engine_data_structures.common_types import CVector3D, Float
 from mercury_engine_data_structures.construct_extensions.alignment import AlignTo
 from mercury_engine_data_structures.formats.base_resource import BaseResource
 from mercury_engine_data_structures.formats.property_enum import PropertyEnumDoubleUnsafe
@@ -40,7 +35,7 @@ def _rebuild_flags(ctx):
 def _update_next_kfv(ctx):
     ctx._._._._next_kfv_offset = ctx._new_eof
 
-BcsklaKFVStruct = Struct(
+KeyFramedValues_Dread = Struct(
     timing_type=Int16ul,
     count=Rebuild(Int16ul, construct.len_(this.timings)),
     timings=IfThenElse(
@@ -54,33 +49,80 @@ BcsklaKFVStruct = Struct(
     _new_kfv=Computed(_update_next_kfv)
 )
 
-BcsklaBoneTrackStruct = Struct(
-    bone_hash=PropertyEnumDoubleUnsafe,
+KeyFramedValues_SR = Struct(
+    count=Rebuild(Int16ul, lambda ctx: 0 if isinstance(ctx.data, float) else len(ctx.data)),
+    _data_type = Const(2, Int16ul),
+    data = IfThenElse(
+        this.count == 0,
+        FocusedSeq(
+            "value",
+            time = Const(0, Int32ul),
+            value = Float
+        ),
+        Array(this.count,
+            Struct(timing=Float, values=CVector3D)
+        )
+    ),
+    _new_eof=Tell,
+    _new_kfv=Computed(_update_next_kfv)
+
+)
+
+BoneTrack_Dread = Struct(
+    bone_name=PropertyEnumDoubleUnsafe,
     flags=Rebuild(Int32ul, _rebuild_flags),
     offset=Tell,
     data=Array(9, IfThenElse(
+        # if the flag for an index is zero, the track
         2 ** this._index & this.flags == 0,
         Float,
         FocusedSeq(
             "kfv",
             off = Rebuild(Int32ul, lambda ctx: ctx._._._next_kfv_offset - ctx._.offset),
-            kfv = Pointer(this.off + this._.offset, BcsklaKFVStruct)
+            kfv = Pointer(this.off + this._.offset, KeyFramedValues_Dread)
         )
     ))
 )
 
-BCSKLA = Struct(
+BoneTrack_SR = Struct(
+    bone_hash=Int32ul, # TODO have a property CrcAdapter for sr lmao
+    offset=Tell,
+    data=Array(9, FocusedSeq(
+        "kfv",
+        off = Rebuild(Int32ul, lambda ctx: 0 if isinstance(ctx.kfv, float) else ctx._._._next_kfv_offset),
+        kfv = IfThenElse(
+            this.off > 0, 
+            Pointer(this.off, KeyFramedValues_SR),
+            IfThenElse(lambda ctx: ctx._index < 6, Computed(0.0), Computed(1.0))
+        )
+    ))
+)
+
+BCSKLA_DREAD = Struct(
     _magic = Const(b"MANM"),
     ver=Const(0x000A0001, Int32ul),
     unk=Int32ul,
     frame_count=Float,
     track_count=Rebuild(Int32ul, construct.len_(this.tracks)),
-    _padding=AlignTo(8, b"\xff"),
+    _padding=If(this.track_count > 0, AlignTo(8, b"\xff")),
     _next_kfv_offset=Computed(this.track_count * 0x30 + 0x18),
-    tracks=Array(this.track_count, BcsklaBoneTrackStruct)
+    tracks=Array(this.track_count, BoneTrack_Dread)
+)
+
+BCSKLA_SR = Struct(
+    _magic = Const(b"MANM"),
+    ver = Const(0x00060001, Int32ul),
+    unk = Int32ul,
+    frame_count=Float,
+    track_count=Rebuild(Int32ul, construct.len_(this.tracks)),
+    _next_kfv_offset=Computed(this.track_count * 0x28 + 0x14),
+    tracks = Array(this.track_count, BoneTrack_SR)
 )
 
 class Bcskla(BaseResource):
     @classmethod
     def construct_class(cls, target_game: Game) -> Construct:
-        return BCSKLA
+        if target_game == Game.DREAD:
+            return BCSKLA_DREAD
+        elif target_game == Game.SAMUS_RETURNS:
+            return BCSKLA_SR
--- tests/formats/test_bcskla.py
+++ tests/formats/test_bcskla.py
@@ -1,12 +1,22 @@
 import pytest
 from tests.test_lib import parse_build_compare_editor
 
-from mercury_engine_data_structures import dread_data
+from mercury_engine_data_structures import dread_data, samus_returns_data
 from mercury_engine_data_structures.formats.bcskla import Bcskla
 
 all_dread_bcskla = [name for name in dread_data.all_name_to_asset_id().keys()
                    if name.endswith(".bcskla")]
 
+all_sr_bcskla = [name for name in samus_returns_data.all_name_to_asset_id().keys()
+                 if name.endswith(".bcskla")]
+
 @pytest.mark.parametrize("bcskla_path", all_dread_bcskla)
-def test_bcskla(dread_file_tree, bcskla_path):
+def test_bcskla_dread(dread_file_tree, bcskla_path):
     parse_build_compare_editor(Bcskla, dread_file_tree, bcskla_path)
+
+@pytest.mark.parametrize("bcskla_path", all_sr_bcskla)
+def test_bcskla_sr(samus_returns_tree, bcskla_path):
+    try:
+        parse_build_compare_editor(Bcskla, samus_returns_tree, bcskla_path)
+    except FileNotFoundError:
+        pytest.skip(f"File {bcskla_path} does not exist!")
