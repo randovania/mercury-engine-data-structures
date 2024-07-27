f7f3bc5ec61bedec70b3a159588eb1164c1415a1
Steven Franklin <steve11sjf@gmail.com>
2024-07-27 03:23:53 -0500

    Added bnvib
    - standard NX library format, already documented online

--- src/mercury_engine_data_structures/formats/__init__.py
+++ src/mercury_engine_data_structures/formats/__init__.py
@@ -30,6 +30,7 @@ from mercury_engine_data_structures.formats.bmsnav import Bmsnav
 from mercury_engine_data_structures.formats.bmssd import Bmssd
 from mercury_engine_data_structures.formats.bmtre import Bmtre
 from mercury_engine_data_structures.formats.bmtun import Bmtun
+from mercury_engine_data_structures.formats.bnvib import Bnvib
 from mercury_engine_data_structures.formats.brem import Brem
 from mercury_engine_data_structures.formats.bres import Bres
 from mercury_engine_data_structures.formats.brev import Brev
@@ -80,6 +81,7 @@ ALL_FORMATS = {
     "BMSLINK": Bmslink,
     "BMSMD": Bmsmd,
     "BMTRE": Bmtre,
+    "BNVIB": Bnvib,
     "BRSA": Brsa,
     "BREM": Brem,
     "BRES": Bres,
---
+++ src/mercury_engine_data_structures/formats/bnvib.py
@@ -0,0 +1,38 @@
+from enum import Enum
+
+import construct
+from construct.core import (
+    Byte,
+    Const,
+    Construct,
+    If,
+    Int16ul,
+    Int32ul,
+    PrefixedArray,
+    Struct,
+)
+
+from mercury_engine_data_structures.adapters.enum_adapter import EnumAdapter
+from mercury_engine_data_structures.formats.base_resource import BaseResource
+from mercury_engine_data_structures.game_check import Game
+
+
+class VibrationType(Enum):
+    NORMAL = 4
+    LOOP = 12
+    LOOPWAIT = 16
+
+BNVIB = Struct(
+    "vibration_type" / EnumAdapter(VibrationType, Int32ul),
+    "_magic" / Const(3, Int16ul),
+    "sample_rate" / Int16ul,
+    "loop_start" / If(construct.this.vibration_type != VibrationType.NORMAL, Int32ul),
+    "loop_end" / If(construct.this.vibration_type != VibrationType.NORMAL, Int32ul),
+    "loop_wait" / If(construct.this.vibration_type == VibrationType.LOOPWAIT, Int32ul),
+    "data" / PrefixedArray(Int32ul, Byte)
+)
+
+class Bnvib(BaseResource):
+    @classmethod
+    def construct_class(cls, target_game: Game) -> Construct:
+        return BNVIB
