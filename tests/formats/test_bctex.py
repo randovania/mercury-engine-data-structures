6ec53966c09b51bc859ca1343dfacdd345548e1b
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2024-08-20 13:23:47 +0300

    Exclude a file that takes 10s to build

--- tests/formats/test_bctex.py
+++ tests/formats/test_bctex.py
@@ -4,6 +4,11 @@ from tests.test_lib import parse_build_compare_editor_parsed
 from mercury_engine_data_structures import dread_data, samus_returns_data
 from mercury_engine_data_structures.formats.bctex import Bctex
 
+dread_exclusions = [
+    # exceptionally slow file
+    "textures/system/fx/tests/watertank/textures/watertank_col.bctex",
+]
+
 # these are existing textures that bytematch existing ones. likely put in the bossrush pkgs for performance.
 dread_210_ignore = [
     "actors/characters/centralunitcave/fx/textures/centralunit_flash.bctex",
@@ -1396,7 +1401,9 @@ sr_missing = [
 ]
 
 
-@pytest.mark.parametrize("bctex_path", dread_data.all_files_ending_with(".bctex", dread_210_ignore + dread_210_only))
+@pytest.mark.parametrize(
+    "bctex_path", dread_data.all_files_ending_with(".bctex", dread_210_ignore + dread_210_only + dread_exclusions)
+)
 def test_compare_dread_100(dread_tree_100, bctex_path):
     parse_build_compare_editor_parsed(Bctex, dread_tree_100, bctex_path)
 
