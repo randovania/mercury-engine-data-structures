d77c1c0bc875d23ec4ff0eba150f0ce0b22c8ff7
Steven Franklin <steve11sjf@gmail.com>
2024-09-08 13:06:24 -0500

    fixes
    - uses rebuild for some more fields
    - enum for FileId.file_type
    - renamed a size to length
    - removed a debugger
    - added additional 2.1.0 data

--- src/mercury_engine_data_structures/formats/bmsstoc.py
+++ src/mercury_engine_data_structures/formats/bmsstoc.py
@@ -1,3 +1,5 @@
+import enum
+
 import construct
 from construct.core import (
     Array,
@@ -5,6 +7,7 @@ from construct.core import (
     Check,
     Const,
     Construct,
+    Enum,
     Int24ul,
     Int32ul,
     Int64ul,
@@ -18,17 +21,27 @@ from mercury_engine_data_structures.formats.base_resource import BaseResource
 from mercury_engine_data_structures.formats.property_enum import PropertyEnum
 from mercury_engine_data_structures.game_check import Game
 
+
+class FileTypeEnum(enum.Enum):
+    SOUND = 1
+    SOUNDGROUP = 2
+    BANK = 3
+    PLAYER = 4
+    WAVE_ARCHIVE = 5
+    GROUP = 6
+
+
 FileId = Struct(
     "file_idx" / Int24ul,
-    "file_type" / construct.Enum(Byte, sound=1, soundgroup=2, bank=3, player=4, wavearchive=5, group=6),
+    "file_type" / Enum(Byte, FileTypeEnum),
 )
 
 WaveArchiveData = Struct(
     Const(0x20, Int32ul),
     "wav_name" / PropertyEnum,
     "file_id" / FileId,
-    "warc_index" / Int32ul, # index of the WARC listed in BFSAR
-    "idx" / Int32ul, # 0 to max wave archives
+    "warc_index" / Int32ul,  # index of the WARC listed in BFSAR (starts offset from 0)
+    "idx" / Rebuild(Int32ul, lambda ctx: ctx._index),  # 0 to max wave archives
     Check(construct.this._index == construct.this.idx),
     Const(0, Int32ul),
     Const(0, Int32ul),
@@ -38,30 +51,35 @@ WaveArchiveData = Struct(
 SoundStreams = Struct(
     Const(0x10, Int32ul),
     "fstm_name" / PropertyEnum,
-    "file_id" / FileId, # 0 to max sound streams
+    "file_id"
+    / Rebuild(
+        FileId,
+        lambda ctx: construct.Container(file_idx=ctx._index, file_type=ctx.file_id.file_type),  # 0 to max sound streams
+    ),
     Check(construct.this._index == construct.this.file_id.file_idx),
-    "stream_index" / Int32ul, # index in bfsar
+    "stream_index" / Int32ul,  # index in bfsar
 )
 
 GroupData = Struct(
     Const(0xB0, Int32ul),
     "group_crc" / PropertyEnum,
     "file_id" / FileId,
-    "file_size" / Int32ul, # ==  length of romfs:/packs/sounds/<group>.bfgrp
+    "file_size" / Int32ul,  # ==  length of romfs:/packs/sounds/<group>.bfgrp
     Const(0, Int64ul),
     "group" / PaddedString(0x80, "ascii"),
-    "_size" / Rebuild(Int32ul, lambda ctx: len(ctx.wavs)),
+    "_len" / Rebuild(Int32ul, lambda ctx: len(ctx.wavs)),
     Const(0, Int32ul),
-    Const(b"\x0D\xF0\xAD\xBA" * 4), # 16B of BAADF00D magic from heap alloc
-    "wavs" / Array(construct.this._size, PropertyEnum),
+    Const(b"\x0d\xf0\xad\xba" * 4),  # 16B of BAADF00D magic from heap alloc
+    "wavs" / Array(construct.this._len, PropertyEnum),
 )
 
 BMSSTOC = Struct(
     "wave_archives" / PrefixedArray(Int32ul, WaveArchiveData),
     "sound_streams" / PrefixedArray(Int32ul, SoundStreams),
-    "groups" / construct.Debugger(PrefixedArray(Int32ul, GroupData)),
+    "groups" / PrefixedArray(Int32ul, GroupData),
     construct.Terminated,
 )
+
 
 class Bmsstoc(BaseResource):
     @classmethod
--- tests/formats/test_bmsstoc.py
+++ tests/formats/test_bmsstoc.py
@@ -2,6 +2,12 @@ from tests.test_lib import parse_build_compare_editor
 
 from mercury_engine_data_structures.formats.bmsstoc import Bmsstoc
 
+BMSSTOC_PATH = "system/snd/sm_sounddefinitions_packed.bmsstoc"
 
-def test_bmsstoc(dread_file_tree):
-    parse_build_compare_editor(Bmsstoc, dread_file_tree, "system/snd/sm_sounddefinitions_packed.bmsstoc")
+
+def test_bmsstoc_100(dread_tree_100):
+    parse_build_compare_editor(Bmsstoc, dread_tree_100, BMSSTOC_PATH)
+
+
+def test_bmsstoc_210(dread_tree_210):
+    parse_build_compare_editor(Bmsstoc, dread_tree_210, BMSSTOC_PATH)
