8954d9b7e9dfa261aa2e47e669182e9d03d21951
Mayberry <mayberryzoom@gmail.com>
2024-10-12 16:41:29 -0400

    Fix collision camera names in tests

--- tests/formats/test_brfld.py
+++ tests/formats/test_brfld.py
@@ -33,14 +33,14 @@ def test_dread_brfld_210(dread_tree_210, brfld_path):
 def test_add_actor_to_actor_groups(dread_tree_100):
     scenario = dread_tree_100.get_file("maps/levels/c10_samus/s010_cave/s010_cave.brfld", Brfld)
 
-    scenario.add_actor_to_actor_groups("collision_camera_000", "breakabletilegroup_000", "breakables")
-    assert scenario.is_actor_in_group("eg_collision_camera_000", "breakabletilegroup_000", "breakables")
+    scenario.add_actor_to_actor_groups("eg_collision_camera_000", "breakabletilegroup_000", "breakables")
+    assert scenario.is_actor_in_group("eg_collision_camera_000_Default", "breakabletilegroup_000", "breakables")
 
     scenario.add_actor_to_actor_groups(
-        "ssg_collision_camera_000_Default", "Pos_C_LavaWindow_06", actor_layer_name=ActorLayer.SOUNDS
+        "ssg_collision_camera_000", "Pos_C_LavaWindow_06", actor_layer=ActorLayer.SOUNDS
     )
     assert scenario.is_actor_in_group(
-        "ssg_collision_camera_000_Default", "Pos_C_LavaWindow_06", actor_layer_name=ActorLayer.SOUNDS
+        "ssg_collision_camera_000_Default", "Pos_C_LavaWindow_06", actor_layer=ActorLayer.SOUNDS
     )
 
     scenario.add_actor_to_actor_groups(
