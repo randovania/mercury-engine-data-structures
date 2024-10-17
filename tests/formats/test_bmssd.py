4119b784090e4999abb0fea587b33edaf82e2113
Steven Franklin <steve11sjf@gmail.com>
2024-10-17 16:10:53 -0500

    adjusted tests

--- tests/formats/test_bmssd.py
+++ tests/formats/test_bmssd.py
@@ -22,22 +22,6 @@ bossrush_assets = [
     "maps/levels/c10_samus/s212_bossrush_commander/s212_bossrush_commander.bmssd",
 ]
 
-sr_missing = [
-    "maps/levels/c10_samus/s901_alpha/s901_alpha.bmssd",
-    "maps/levels/c10_samus/s902_gamma/s902_gamma.bmssd",
-    "maps/levels/c10_samus/s903_zeta/s903_zeta.bmssd",
-    "maps/levels/c10_samus/s904_omega/s904_omega.bmssd",
-    "maps/levels/c10_samus/s905_arachnus/s905_arachnus.bmssd",
-    "maps/levels/c10_samus/s905_queen/s905_queen.bmssd",
-    "maps/levels/c10_samus/s906_metroid/s906_metroid.bmssd",
-    "maps/levels/c10_samus/s907_manicminerbot/s907_manicminerbot.bmssd",
-    "maps/levels/c10_samus/s908_manicminerbotrun/s908_manicminerbotrun.bmssd",
-    "maps/levels/c10_samus/s909_ridley/s909_ridley.bmssd",
-    "maps/levels/c10_samus/s910_gym/s910_gym.bmssd",
-    "maps/levels/c10_samus/s911_swarmgym/s911_swarmgym.bmssd",
-    "maps/levels/c10_samus/s920_traininggallery/s920_traininggallery.bmssd",
-]
-
 
 @pytest.mark.parametrize("bmssd_path", dread_data.all_files_ending_with(".bmssd", bossrush_assets))
 def test_compare_bmssd_dread_100(dread_tree_100, bmssd_path):
@@ -49,7 +33,7 @@ def test_compare_dread_210(dread_tree_210, bmssd_path):
     parse_build_compare_editor_parsed(Bmssd, dread_tree_210, bmssd_path)
 
 
-@pytest.mark.parametrize("bmssd_path", samus_returns_data.all_files_ending_with(".bmssd", sr_missing))
+@pytest.mark.parametrize("bmssd_path", samus_returns_data.all_files_ending_with(".bmssd"))
 def test_compare_bmssd_msr(samus_returns_tree, bmssd_path):
     parse_build_compare_editor_parsed(Bmssd, samus_returns_tree, bmssd_path)
 
