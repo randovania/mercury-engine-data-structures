b6ca384b310757a0e1fe39d4e04d520efd080506
dyceron <dycerontv@gmail.com>
2025-01-11 18:19:09 -0500

    Add classes to BMSMSD

--- src/mercury_engine_data_structures/formats/bmsmsd.py
+++ src/mercury_engine_data_structures/formats/bmsmsd.py
@@ -13,6 +13,7 @@ from construct.core import (
     Float32l,
     Int32sl,
     Int32ul,
+    ListContainer,
     Struct,
 )
 
@@ -21,11 +22,15 @@ from mercury_engine_data_structures.common_types import (
     CVector2D,
     CVector3D,
     StrId,
+    Vec2,
+    Vec3,
     VersionAdapter,
     make_vector,
 )
 
 if TYPE_CHECKING:
+    from enum import IntEnum
+
     from mercury_engine_data_structures.game_check import Game
 
 TileBorders = FlagsEnum(
@@ -65,7 +70,7 @@ BMSMSD = Struct(
     "_magic" / Const(b"MMSD"),
     "version" / VersionAdapter("1.7.0"),
     "scenario" / StrId,
-    "tile_size" / construct.Array(2, Float32l),
+    "tile_size" / CVector2D,
     "x_tiles" / Int32sl,
     "y_tiles" / Int32sl,
     "map_dimensions" / Struct(
@@ -96,11 +101,118 @@ BMSMSD = Struct(
 )  # fmt: skip
 
 
+class IconProperties:
+    def __init__(self, raw: Container) -> None:
+        self._raw = raw
+
+    @property
+    def actor_name(self) -> str:
+        return self._raw.actor_name
+
+    @actor_name.setter
+    def actor_name(self, value: str) -> None:
+        self._raw.actor_name = value
+
+    @property
+    def clear_condition(self) -> str:
+        return self._raw.clear_condition
+
+    @clear_condition.setter
+    def clear_condition(self, value: str) -> None:
+        self._raw.clear_condition = value
+
+    @property
+    def icon(self) -> str:
+        return self._raw.icon
+
+    @icon.setter
+    def icon(self, value: str) -> None:
+        self._raw.icon = value
+
+    @property
+    def icon_priority(self) -> str:
+        return self._raw.icon_priority
+
+    @icon_priority.setter
+    def icon_priority(self, value: str) -> None:
+        self._raw.icon_priority = value
+
+    @property
+    def coordinates(self) -> Vec3:
+        return self._raw.coordinates
+
+    @coordinates.setter
+    def coordinates(self, value: Vec3) -> None:
+        self._raw.coordinates = value
+
+
+    def _get_icon_properties(self) -> Container:
+        icon = Container({
+            "actor_name": self.actor_name,
+            "clear_condition": self.clear_condition,
+            "icon": self.icon,
+            "icon_priority": self.icon_priority,
+            "coordinates": self.coordinates,
+        })
+        return icon
+
+
+
+class TileProperties:
+    def __init__(self, raw: Container) -> None:
+        self._raw = raw
+
+    @property
+    def tile_coordinates(self) -> list[int]:
+        return self._raw.tile_coordinates
+
+    @tile_coordinates.setter
+    def tile_coordinates(self, value: list[int]) -> None:
+        self._raw.tile_coordinates = value
+
+    @property
+    def dimension(self) -> dict[Vec2, Vec2]:
+        return self._raw.tile_dimension
+
+    @property
+    def tile_type(self) -> IntEnum:
+        return self._raw.tile_type
+
+    @tile_type.setter
+    def tile_type(self, value: IntEnum):
+        self._raw.tile_type = value
+
+    @property
+    def icons(self) -> ListContainer:
+        return self._raw.icons
+
+    def get_icon(self, icon_idx: int = 0) -> IconProperties:
+        return IconProperties._get_icon_properties(self.icons[icon_idx])
+
+    def add_icon(
+        self,
+        actor_name: str,
+        clear_condition: str,
+        icon: str,
+        icon_priority: str,
+        coordinates: Vec3,
+    ) -> Container:
+        new_icon = Container({
+            "actor_name": actor_name,
+            "clear_condition": clear_condition,
+            "icon": icon,
+            "icon_priority": icon_priority,
+            "coordinates": coordinates
+        })
+
+        self.icons.append(new_icon)
+
+
 class Bmsmsd(BaseResource):
     @classmethod
     @functools.lru_cache
     def construct_class(cls, target_game: Game) -> Construct:
         return BMSMSD
 
-    def get_tile(self, tile_idx: int) -> Container:
-        return self.raw.tiles[tile_idx]
+    def get_tile(self, tile_idx: int) -> TileProperties:
+        return TileProperties(self.raw.tiles[tile_idx])
--- tests/formats/test_bmsmsd.py
+++ tests/formats/test_bmsmsd.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
 import pytest
+from construct import Container
 from tests.test_lib import parse_build_compare_editor
 
 from mercury_engine_data_structures import samus_returns_data
-from mercury_engine_data_structures.formats.bmsmsd import Bmsmsd, TileType
+from mercury_engine_data_structures.common_types import Vec3
+from mercury_engine_data_structures.formats.bmsmsd import Bmsmsd, IconPriority, TileType
 
 
 @pytest.mark.parametrize("bmsmsd_path", samus_returns_data.all_files_ending_with(".bmsmsd"))
@@ -26,3 +28,28 @@ def test_get_tile(surface_bmsmsd: Bmsmsd):
 
     tile = surface_bmsmsd.get_tile(25)
     assert tile.tile_type == TileType.NORMAL
+
+
+def test_get_icon(surface_bmsmsd: Bmsmsd):
+    icon = surface_bmsmsd.get_tile(4).get_icon()
+    assert icon.actor_name == "LE_Item_001"
+    assert icon.clear_condition == ""
+    assert icon.icon == "item_missiletank"
+    assert icon.icon_priority == IconPriority.ACTOR
+    assert icon.coordinates == Vec3(-5500.0, -9700.0, 0.0)
+
+
+def test_add_icon(surface_bmsmsd: Bmsmsd):
+    tile = surface_bmsmsd.get_tile(10)
+    assert tile is not None
+
+    tile.add_icon("LE_Test_Icon", "CollectItem", "itemsphere", IconPriority.ACTOR, Vec3(100.0, 100.0, 0.0))
+
+    new_icon = Container({
+        "actor_name": "LE_Test_Icon",
+        "clear_condition": "CollectItem",
+        "icon": "itemsphere",
+        "icon_priority": IconPriority.ACTOR,
+        "coordinates": Vec3(100.0, 100.0, 0.0)
+    })
+    assert tile.get_icon(1) == new_icon
