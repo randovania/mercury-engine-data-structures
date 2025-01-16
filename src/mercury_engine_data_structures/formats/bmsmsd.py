d86ec81dd77e90e24c17b631f0a9c3b77e0674b8
dyceron <dycerontv@gmail.com>
2025-01-16 18:37:56 -0500

    Address more review and add new adaptor and common_type

--- src/mercury_engine_data_structures/formats/bmsmsd.py
+++ src/mercury_engine_data_structures/formats/bmsmsd.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import functools
+from enum import Enum
 from typing import TYPE_CHECKING
 
 import construct
@@ -8,15 +9,16 @@ from construct.core import (
     Const,
     Construct,
     Container,
-    Enum,
-    FlagsEnum,
     Int32sl,
     Int32ul,
     Struct,
 )
 
+from mercury_engine_data_structures.adapters.enum_adapter import EnumAdapter
+from mercury_engine_data_structures.adapters.flagsenum_adapter import FlagsEnumAdapter
 from mercury_engine_data_structures.base_resource import BaseResource
 from mercury_engine_data_structures.common_types import (
+    BoundingBox2D,
     CVector2D,
     CVector3D,
     StrId,
@@ -27,41 +29,40 @@ from mercury_engine_data_structures.common_types import (
 )
 
 if TYPE_CHECKING:
-    from enum import IntEnum
 
     from mercury_engine_data_structures.game_check import Game
 
-TileBorders = FlagsEnum(
-    Int32sl,
-    TOP=1,
-    BOTTOM=2,
-    LEFT=4,
-    RIGHT=8,
-    OPEN_TOP=16,
-    OPEN_BOTTOM=32,
-    OPEN_LEFT=64,
-    OPEN_RIGHT=128,
-)
+class TileBorder(int, Enum):
+    TOP = 1
+    BOTTOM = 2
+    LEFT = 4
+    RIGHT = 8
+    OPEN_TOP = 16
+    OPEN_BOTTOM = 32
+    OPEN_LEFT = 64
+    OPEN_RIGHT = 128
 
-TileType = Enum(
-    Int32ul,
-    NORMAL=1,
-    HEAT=2,
-    ACID=4,
-    ACID_RISE=8,
-    ACID_FALL=12,
-)
+TileBorderConstruct = FlagsEnumAdapter(TileBorder, Int32sl)
 
-IconPriority = Enum(
-    Int32sl,
-    METROID=-1,
-    ACTOR=0,
-    SHIP=1,
-    ENERGY_CLOUD=2,
-    DOOR=3,
-    CHOZO_SEAL=4,
-    HIDDEN_ITEM=5,
-)
+class TileType(int, Enum):
+    NORMAL = 1
+    HEAT = 2
+    ACID = 4
+    ACID_RISE = 8
+    ACID_FALL = 12
+
+TileTypeConstruct = EnumAdapter(TileType, Int32ul)
+
+class IconPriority(int, Enum):
+    METROID = -1
+    ACTOR = 0
+    SHIP = 1
+    ENERGY_CLOUD = 2
+    DOOR = 3
+    CHOZO_SEAL = 4
+    HIDDEN_ITEM = 5
+
+IconPriorityConstruct = EnumAdapter(IconPriority, Int32sl)
 
 # BMSMSD
 BMSMSD = Struct(
@@ -71,25 +72,19 @@ BMSMSD = Struct(
     "tile_size" / CVector2D,
     "x_tiles" / Int32sl,
     "y_tiles" / Int32sl,
-    "map_dimensions" / Struct(
-        "bottom_left" / CVector2D,
-        "top_right" / CVector2D,
-    ),
+    "map_dimensions" / BoundingBox2D,
     "tiles" / make_vector(
         Struct(
             "tile_coordinates" / construct.Array(2, Int32sl),
-            "tile_dimensions" / Struct(
-                "bottom_left" / CVector2D,
-                "top_right" / CVector2D,
-            ),
-            "tile_borders" / TileBorders,
-            "tile_type" / TileType,
+            "tile_dimensions" / BoundingBox2D,
+            "tile_borders" / TileBorderConstruct,
+            "tile_type" / TileTypeConstruct,
             "icons" / make_vector(
                 Struct(
                     "actor_name" / StrId,
                     "clear_condition" / StrId,
                     "icon" / StrId,
-                    "icon_priority" / IconPriority,
+                    "icon_priority" / IconPriorityConstruct,
                     "coordinates" / CVector3D,
                 )
             ),
@@ -128,11 +123,11 @@ class IconProperties:
         self._raw.icon = value
 
     @property
-    def icon_priority(self) -> str:
+    def icon_priority(self) -> IconPriority:
         return self._raw.icon_priority
 
     @icon_priority.setter
-    def icon_priority(self, value: str) -> None:
+    def icon_priority(self, value: IconPriority) -> None:
         self._raw.icon_priority = value
 
     @property
@@ -142,18 +137,6 @@ class IconProperties:
     @coordinates.setter
     def coordinates(self, value: Vec3) -> None:
         self._raw.coordinates = value
-
-    def _get_icon_properties(self) -> Container:
-        icon = Container(
-            {
-                "actor_name": self.actor_name,
-                "clear_condition": self.clear_condition,
-                "icon": self.icon,
-                "icon_priority": self.icon_priority,
-                "coordinates": self.coordinates,
-            }
-        )
-        return icon
 
 
 class TileProperties:
@@ -177,26 +160,23 @@ class TileProperties:
         self._raw.tile_dimensions = value
 
     @property
-    def tile_borders(self) -> dict[FlagsEnum]:
+    def tile_borders(self) -> dict[TileBorder, bool]:
         return self._raw.tile_borders
 
+    @tile_borders.setter
+    def tile_borders(self, border_type: dict[TileBorder, bool], value: bool) -> None:
+        self._raw.tile_borders[border_type] = value
+
     @property
-    def tile_type(self) -> IntEnum:
+    def tile_type(self) -> TileType:
         return self._raw.tile_type
 
     @tile_type.setter
-    def tile_type(self, value: IntEnum):
+    def tile_type(self, value: TileType):
         self._raw.tile_type = value
 
-    @property
-    def icons(self) -> list:
-        return self._raw.icons
-
-    def update_tile_borders(self, border_type: FlagsEnum, value: bool) -> None:
-        self._raw.tile_borders[border_type] = value
-
     def get_icon(self, icon_idx: int = 0) -> IconProperties:
-        return IconProperties._get_icon_properties(self.icons[icon_idx])
+        return IconProperties(self._raw.icons[icon_idx])
 
     def add_icon(
         self,
@@ -216,7 +196,10 @@ class TileProperties:
             }
         )
 
-        self.icons.append(new_icon)
+        self._raw.icons.append(new_icon)
+
+    def remove_icon(self, icon_idx: int = 0) -> None:
+        self._raw.icons.pop(icon_idx)
 
 
 class Bmsmsd(BaseResource):
--- tests/formats/test_bmsmsd.py
+++ tests/formats/test_bmsmsd.py
@@ -1,13 +1,11 @@
 from __future__ import annotations
 
-import copy
-
 import pytest
 from tests.test_lib import parse_build_compare_editor
 
 from mercury_engine_data_structures import samus_returns_data
 from mercury_engine_data_structures.common_types import Vec2, Vec3
-from mercury_engine_data_structures.formats.bmsmsd import Bmsmsd, IconPriority, TileBorders, TileType
+from mercury_engine_data_structures.formats.bmsmsd import Bmsmsd, IconPriority, TileBorder, TileType
 
 
 @pytest.mark.parametrize("bmsmsd_path", samus_returns_data.all_files_ending_with(".bmsmsd"))
@@ -25,37 +23,37 @@ def test_get_tile(surface_bmsmsd: Bmsmsd):
     assert tile.tile_coordinates == [48, 5]
 
     tile = surface_bmsmsd.get_tile(0)
-    assert tile.tile_dimensions == {"bottom_left": Vec2(6400.0, -10600.0), "top_right": Vec2(7000.0, -9800.0)}
+    assert tile.tile_dimensions == {"min": Vec2(6400.0, -10600.0), "max": Vec2(7000.0, -9800.0)}
 
     tile = surface_bmsmsd.get_tile(8)
     assert tile.tile_borders == {
-        TileBorders.TOP: True,
-        TileBorders.BOTTOM: True,
-        TileBorders.LEFT: False,
-        TileBorders.RIGHT: True,
-        TileBorders.OPEN_TOP: False,
-        TileBorders.OPEN_BOTTOM: False,
-        TileBorders.OPEN_LEFT: False,
-        TileBorders.OPEN_RIGHT: False,
+        TileBorder.TOP: True,
+        TileBorder.BOTTOM: True,
+        TileBorder.RIGHT: True,
     }
 
     tile = surface_bmsmsd.get_tile(25)
     assert tile.tile_type == TileType.NORMAL
 
     tile = surface_bmsmsd.get_tile(12)
-    assert len(tile.icons) == 2
+    assert tile.get_icon(0) is not None
+    assert tile.get_icon(1) is not None
 
 
 def test_set_tile_properties(surface_bmsmsd: Bmsmsd):
     tile = surface_bmsmsd.get_tile(0)
-    original_tile = copy.deepcopy(tile)
 
     tile.tile_coordinates = [30, 20]
-    tile.tile_dimensions = {"bottom_left": Vec2(10000.0, -1000.0), "top_right": Vec2(50000.0, -29000.0)}
-    tile.update_tile_borders(TileBorders.OPEN_TOP, True)
-    tile.tile_type = TileType.ACID_FALL
+    assert tile.tile_coordinates == [30, 20]
 
-    assert original_tile != tile
+    tile.tile_dimensions = {"min": Vec2(10000.0, -1000.0), "max": Vec2(50000.0, -29000.0)}
+    assert tile.tile_dimensions == {"min": Vec2(10000.0, -1000.0), "max": Vec2(50000.0, -29000.0)}
+
+    tile.tile_borders[TileBorder.OPEN_TOP] = True
+    assert tile.tile_borders[TileBorder.OPEN_TOP] is True
+
+    tile.tile_type = TileType.ACID_FALL
+    assert tile.tile_type is TileType.ACID_FALL
 
 
 def test_get_icon(surface_bmsmsd: Bmsmsd):
@@ -63,7 +61,7 @@ def test_get_icon(surface_bmsmsd: Bmsmsd):
     assert icon.actor_name == "LE_Item_001"
     assert icon.clear_condition == ""
     assert icon.icon == "item_missiletank"
-    assert icon.icon_priority == IconPriority.ACTOR
+    assert icon.icon_priority is IconPriority.ACTOR
     assert icon.coordinates == Vec3(-5500.0, -9700.0, 0.0)
 
 
@@ -73,11 +71,14 @@ def test_add_icon(surface_bmsmsd: Bmsmsd):
 
     tile.add_icon("LE_Test_Icon", "CollectItem", "itemsphere", IconPriority.ACTOR, Vec3(100.0, 100.0, 0.0))
 
-    new_icon = {
-        "actor_name": "LE_Test_Icon",
-        "clear_condition": "CollectItem",
-        "icon": "itemsphere",
-        "icon_priority": IconPriority.ACTOR,
-        "coordinates": Vec3(100.0, 100.0, 0.0),
-    }
-    assert tile.get_icon(1) == new_icon
+    icon = tile.get_icon(1)
+    assert icon.actor_name == "LE_Test_Icon"
+    assert icon.clear_condition == "CollectItem"
+    assert icon.icon == "itemsphere"
+    assert icon.icon_priority is IconPriority.ACTOR
+    assert icon.coordinates == Vec3(100.0, 100.0, 0.0)
+
+
+def test_remove_icon(surface_bmsmsd: Bmsmsd):
+    tile = surface_bmsmsd.get_tile(1)
+    tile.remove_icon(0)
