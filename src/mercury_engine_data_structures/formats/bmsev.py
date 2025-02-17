3e222dbb07a5cc5936ccfac9df47ad0720d44800
Thanatos <thanatosstr89@gmail.com>
2025-02-17 19:33:38 +0100

    Change to make_dict

--- src/mercury_engine_data_structures/formats/bmsev.py
+++ src/mercury_engine_data_structures/formats/bmsev.py
@@ -1,4 +1,7 @@
+from __future__ import annotations
+
 import functools
+from typing import TYPE_CHECKING
 
 from construct import (
     Array,
@@ -9,19 +12,24 @@ from construct import (
     Terminated,
 )
 
-from mercury_engine_data_structures.common_types import StrId, VersionAdapter, make_vector
-from mercury_engine_data_structures.formats.base_resource import BaseResource
-from mercury_engine_data_structures.game_check import Game
+from mercury_engine_data_structures.base_resource import BaseResource
+from mercury_engine_data_structures.common_types import VersionAdapter, make_dict
+
+if TYPE_CHECKING:
+    from mercury_engine_data_structures.game_check import Game
 
 BMSEV = Struct(
     "magic" / Const(b"MSEV"),
     "version" / VersionAdapter(),
-    "elements" / make_vector(Struct(
-        "name" / StrId,
-        "unknown_floats" / Array(59, Float32l),
-    )),
+    "elements"
+    / make_dict(
+        Struct(
+            "unknown_floats" / Array(59, Float32l),
+        )
+    ),
     Terminated,
 ).compile()
+
 
 class Bmsev(BaseResource):
     @classmethod
