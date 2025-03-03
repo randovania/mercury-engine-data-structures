89c60016d618d77b61f8aaa98ad5771a7aa7f4be
Mayberry <mayberryzoom@gmail.com>
2025-03-02 20:58:12 -0500

    Ruff fixes

--- src/mercury_engine_data_structures/formats/brem.py
+++ src/mercury_engine_data_structures/formats/brem.py
@@ -2,13 +2,12 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from construct import Container
-
 from mercury_engine_data_structures.base_resource import BaseResource
 from mercury_engine_data_structures.formats import standard_format
 
 if TYPE_CHECKING:
     import construct
+    from construct import Container
 
     from mercury_engine_data_structures.game_check import Game
 
